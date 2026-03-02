481edc2a7ce918b67ac94da6383801fd55ba83d0
rush2pranav <pranav.p.phulpagar@gmail.com>
2026-03-02 00:11:19 -0600

    property declaration handler and UI changes

---
+++ ConfigEditor.App/Converters/SafeNumberConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ConfigEditor.App.Converters
+{
+    // to connvert between strings and numbers in text box bindings
+    public class SafeIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value?.ToString() ?? "0";
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string s && int.TryParse(s, out int result))
+                return result;
+            return 0;
+        }
+    }
+
+    public class SafeDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value?.ToString() ?? "0";
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string s && double.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out double result))
+                return result;
+            return 0.0;
+        }
+    }
+}
